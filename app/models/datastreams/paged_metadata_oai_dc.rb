9660301d3e0bb963d31b2d9d80e1c8823504bc24
Mark H. Wood <mwood@IUPUI.Edu>
2014-04-15 15:35:31 -0400

    [HPT-82] Move PagedObject metadata from DC stream to new descMetadata stream

--- app/models/datastreams/paged_metadata_oai_dc.rb
+++ app/models/datastreams/paged_metadata_oai_dc.rb
@@ -1,14 +1,15 @@
-# Represent the Fedora standard DC datastream of a Paged object.
+# Represent an OAI_DC (Dublin Core XML) datastream.
 #--
-# Copyright 2014 Indiana University
+# Copyright 2014 Indiana University.
 
-class PagedMetadataDublinCore < ActiveFedora::OmDatastream
+class PagedMetadataOaiDc < ActiveFedora::OmDatastream
 
   set_terminology do |t|
     t.root(path: 'oai_dc:dc',
       'xmlns:oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/',
       'xmlns:dc' => 'http://purl.org/dc/elements/1.1/')
     t.title(namespace_prefix: 'dc')
+    t.creator(namespace_prefix: 'dc')
   end
 
   def self.xml_template
