4c281583e278f8722b3a59d87a0b6b4505432af9
Andy Smith <andjsmit@iupui.edu>
2014-05-01 14:27:26 -0400

    [HPT-127] Fixes deprecated link_to_function calls.

--- app/assets/javascripts/paged.js
+++ app/assets/javascripts/paged.js
@@ -1,13 +1,18 @@
-function show_add_page_form(){
-    if($('#add_page_form').css('display') == 'none'){
-      $('#add_page_form').slideDown("slow",function(){
-        $('#add_page').toggle();
-        $('#cancel_add_page').toggle();
-      });
-    }else{
-      $('#add_page_form').slideUp("slow",function(){
-        $('#add_page').toggle();
-        $('#cancel_add_page').toggle();  
-      });
-    }
-}
+$(function() {
+   $('.show_add_page_form').click(function() {
+     
+      if($('#add_page_form').css('display') == 'none'){
+        $('#add_page_form').slideDown("slow",function(){
+          $('#add_page').toggle();
+          $('#cancel_add_page').toggle();          
+        });
+        return false;
+      }else{
+        $('#add_page_form').slideUp("slow",function(){
+          $('#add_page').toggle();
+          $('#cancel_add_page').toggle();  
+        });
+        return false;
+      }      
+   });
+});
