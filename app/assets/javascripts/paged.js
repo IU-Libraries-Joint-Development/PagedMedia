fac697284dd48621d7b2d396ebca0ef0ba0a7ce2
Andy Smith <andjsmit@iupui.edu>
2014-05-02 13:34:54 -0400

    Fixes issues with turblinks not loading needed JS. Puts JS in default coffeescript, removing needed for additional JS file.

--- app/assets/javascripts/paged.js
+++ app/assets/javascripts/paged.js
@@ -1,17 +1,17 @@
-$(function() {
-   $('.show_add_page_form').click(function() {     
-      if($('#add_page_form').css('display') == 'none'){
-        $('#add_page_form').slideDown("slow",function(){
-          $('#add_page').toggle();
-          $('#cancel_add_page').toggle();          
-        });
-        return false;
-      }else{
-        $('#add_page_form').slideUp("slow",function(){
-          $('#add_page').toggle();
-          $('#cancel_add_page').toggle();  
-        });
-        return false;
-      }      
-   });
+/*
+$(document).on("click",".show_add_page_form" ,function() {     
+    if($('#add_page_form').css('display') == 'none'){
+      $('#add_page_form').slideDown("slow",function(){
+        $('#add_page').toggle();
+        $('#cancel_add_page').toggle();          
+      });
+      return false;
+    }else{
+      $('#add_page_form').slideUp("slow",function(){
+        $('#add_page').toggle();
+        $('#cancel_add_page').toggle();  
+      });
+      return false;
+    }      
 });
+*/
