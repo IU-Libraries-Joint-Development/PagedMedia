4f2c811f738b08634b84cdc2ae65cf507a9776fc
Mark H. Wood <mwood@IUPUI.Edu>
2014-05-22 17:09:02 -0400

    Add some tests for the attached XML file, fix busted tests, remove empty duplicate test definition file.

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -4,15 +4,22 @@ require 'spec_helper'
 
 describe Page do
 
-  before { @page = Page.new }
+  before(:each) { @page = Page.new }
 
-  subject { @page }
+  it "should have the specified datastreams" do
+    # Check for descMetadata datastream
+    # Check for pageImage datastream
+    # Check for pageOCR datastream
+    # Check for pageXML datastream
+    @page.datastreams.keys.should include "pageXML"
+    @page.pageXML.should be_kind_of ActiveFedora::Datastream
+  end
 
   #FIXME: why do these 3 rspec tests fail, when they work in the console?
 #  it { should respond_to(:descMetadata) }
-#  it { should respond_to(:pageImage) }
-#  it { should respond_to(:pageOCR) }
-#  it { should respond_to(:pageXML) }
+  it { should respond_to(:image_file) }
+  it { should respond_to(:ocr_file) }
+  it { should respond_to(:xml_file) }
 
   it { should respond_to(:logical_number) }
   it { should respond_to(:physical_number) }
