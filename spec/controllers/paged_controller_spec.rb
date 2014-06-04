67cd418a9195dd77707fed3c95e2370d68c86dd5
Mark H. Wood <mwood@IUPUI.Edu>
2014-06-04 12:15:08 -0400

    We had no tests for Paged so supply some pending tests to be fleshed out later

---
+++ spec/controllers/paged_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe PagedsController do
+
+  describe 'GET index' do
+    it 'lists Pageds'
+  end
+
+  describe 'GET show' do
+    it 'displays detail of a Paged'
+  end
+
+  describe 'GET new' do
+    it 'displays the create form'
+  end
+
+  describe 'GET edit' do
+    it 'displays the edit form'
+  end
+
+  describe 'POST create' do
+    it 'stores a new Page'
+  end
+
+  describe 'PUT update' do
+    it 'updates the page somehow'
+  end
+
+  describe 'DELETE destroy' do
+    it 'destroys a Paged'
+  end
+
+end
