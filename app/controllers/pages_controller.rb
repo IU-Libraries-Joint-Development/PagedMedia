991ea448e0232c6d5e6da393823a6633de1f784b
Mark H. Wood <mwood@IUPUI.Edu>
2014-06-04 12:13:04 -0400

    Had to split require, permit as require was returning String not param object

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -94,7 +94,8 @@ class PagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
-      params.require(:page).permit(:logical_number, :physical_number, 
+      params.require(:page)
+      params.permit(:logical_number, :physical_number,
         :image_file, :paged_id, :ocr_file, :xml_file)
     end
 end
