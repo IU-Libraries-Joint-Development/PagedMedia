fed0ccb2cec9499674a0ff162df2dd11e4e7f8f6
Adam Ploshay <aploshay@iu.edu>
2014-10-29 22:49:01 -0400

    Removed allow_cross_domain_access filter

--- app/controllers/pageds_controller.rb
+++ app/controllers/pageds_controller.rb
@@ -1,15 +1,6 @@
 class PagedsController < ApplicationController
   before_action :set_paged, only: [:show, :edit, :update, :destroy, :page, :bookreader]
 
-  # FIXME testing for javascript
-  before_filter :allow_cross_domain_access
-  def allow_cross_domain_access
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
-
   # GET /pageds
   # GET /pageds.json
   def index
