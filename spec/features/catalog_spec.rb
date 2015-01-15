0bb00c7a75c0b0cfe1e63da004a328e04474aae2
Adam Ploshay <adam.ploshay@gmail.com>
2015-01-15 11:26:47 -0500

    Updated feature specs

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -1,31 +1,21 @@
-require 'spec_helper'
- 
-  # To get the routes to work properly,
-  # config.include Rails.application.routes.url_helpers
-  # was added to RSpec.configure do |config|
-  # in spec_helper.rb
-
 describe "Catalog features" do
 
   describe "facet search" do
-
-    before(:all) do
-      @test_newspaper = FactoryGirl.create :paged, :newspaper
-      @test_score = FactoryGirl.create :paged, :score
-    end
+    let!(:test_newspaper) { FactoryGirl.create :paged, :newspaper }
+    let!(:test_score) { FactoryGirl.create :paged, :score }
 
     context "from the main page" do
       before(:each) do
         visit root_path
       end
       it "should list type links" do
-      	within('#facets'){expect(page).to have_link @test_newspaper.type}
-      	within('#facets'){expect(page).to have_link @test_score.type}
+      	within('#facets'){expect(page).to have_link test_newspaper.type}
+      	within('#facets'){expect(page).to have_link test_score.type}
       end
       it "should link to type-specific items" do
-      	within('#facets'){click_link(@test_newspaper.type)}
-        within('#documents'){expect(page).to have_content @test_newspaper.title}
-        within('#documents'){expect(page).not_to have_content @test_score.title}
+      	within('#facets'){click_link(test_newspaper.type)}
+        within('#documents'){expect(page).to have_content test_newspaper.title}
+        within('#documents'){expect(page).not_to have_content test_score.title}
       end
     end
 
@@ -35,56 +25,48 @@ describe "Catalog features" do
         click_button 'Search'
       end
       it "should filter to type-specific search results" do
-        within('#documents'){expect(page).to have_content @test_newspaper.title}
-        within('#documents'){expect(page).to have_content @test_score.title}
-        within('#facets'){click_link(@test_newspaper.type)}
-        within('#documents'){expect(page).to have_content @test_newspaper.title}
-        within('#documents'){expect(page).not_to have_content @test_score.title}
+        within('#documents'){expect(page).to have_content test_newspaper.title}
+        within('#documents'){expect(page).to have_content test_score.title}
+        within('#facets'){click_link(test_newspaper.type)}
+        within('#documents'){expect(page).to have_content test_newspaper.title}
+        within('#documents'){expect(page).not_to have_content test_score.title}
       end
       it "should show unfiltered search results when filter is removed" do
-        within('#facets'){click_link(@test_newspaper.type)}
+        within('#facets'){click_link(test_newspaper.type)}
         within('#appliedParams'){click_link "Remove constraint" }
-        within('#documents'){expect(page).to have_content @test_newspaper.title}
-        within('#documents'){expect(page).to have_content @test_score.title}
+        within('#documents'){expect(page).to have_content test_newspaper.title}
+        within('#documents'){expect(page).to have_content test_score.title}
       end
-    end
-
-    after(:all) do
-      @test_newspaper.delete
-      @test_score.delete
     end
 
   end
 
   describe "term search" do
+    let!(:test_paged) { FactoryGirl.create :paged }
     
-    before(:all) do
-      @test_paged = FactoryGirl.create :paged
-    end
-      
     context "searching everything" do
       it "should find records" do
         visit root_path
         click_button 'Search'
-        within('#documents'){expect(page).to have_content @test_paged.title}
+        within('#documents'){expect(page).to have_content test_paged.title}
       end
     end    
     context "search titles" do
       it "should find records" do
         visit root_path
         select 'Title', from: 'search_field'
-        fill_in 'q', with: @test_paged.title
+        fill_in 'q', with: test_paged.title
         click_button 'Search'
-        within('#documents'){expect(page).to have_content @test_paged.title}
+        within('#documents'){expect(page).to have_content test_paged.title}
       end
     end
     context "search creator" do
       it "should find records" do
         visit root_path
         select 'Creator', from: 'search_field'
-        fill_in 'q', with: @test_paged.creator
+        fill_in 'q', with: test_paged.creator
         click_button 'Search'
-        within('#documents'){expect(page).to have_content @test_paged.title}
+        within('#documents'){expect(page).to have_content test_paged.title}
       end
     end
 
@@ -92,9 +74,9 @@ describe "Catalog features" do
       it "should find records of type generic" do
         visit root_path
         select 'Type', from: 'search_field'
-        fill_in 'q', with: @test_paged.type
+        fill_in 'q', with: test_paged.type
         click_button 'Search'
-        within('#documents'){expect(page).to have_content @test_paged.title}
+        within('#documents'){expect(page).to have_content test_paged.title}
       end
     end
 
@@ -106,10 +88,6 @@ describe "Catalog features" do
       end
     end
     
-    after(:all) do
-      @test_paged.delete
-    end
-
   end
 
   describe 'browse' do
--- spec/features/pageds_spec.rb
+++ spec/features/pageds_spec.rb
@@ -1,14 +1,9 @@
-require 'spec_helper'
-
 describe 'For page listing' do
-
-  before(:all) do
-    @test_paged = create(:paged, :with_pages)
-  end
+  let!(:test_paged) { FactoryGirl.create :paged, :with_pages }
 
   context "when pages are listed" do  
-    specify "they should be ordered according to prev and next page ids" do      
-      visit pageds_path + '/' + @test_paged.pid
+    specify "they should be ordered according to prev and next page ids" do
+      visit pageds_path + '/' + test_paged.pid
       page.body.index("Page 1").should < page.body.index("Page 2")
       page.body.index("Page 2").should < page.body.index("Page 3")
       page.body.index("Page 3").should < page.body.index("Page 4")
@@ -21,7 +16,7 @@ describe 'For page listing' do
       #Find page 3 and remove prev page
       prev_page = ''
       page3 = ''
-      @test_paged.pages.each {|page|
+      test_paged.pages.each {|page|
         if page.logical_number == "Page 3"
           page3 = page
           prev_page = page.prev_page
@@ -29,7 +24,7 @@ describe 'For page listing' do
           page.save!
         end
       }      
-      visit pageds_path + '/' + @test_paged.pid
+      visit pageds_path + '/' + test_paged.pid
       # Return page 3's prev page
       page3.prev_page = prev_page
       page3.save!
@@ -42,7 +37,7 @@ describe 'For page listing' do
       # Find page 3 and redirect it to itself
       next_page = ''
       page3 = ''
-      @test_paged.pages.each {|page|
+      test_paged.pages.each {|page|
         if page.logical_number == "Page 3"
           page3 = page
           next_page = page.next_page
@@ -50,7 +45,7 @@ describe 'For page listing' do
           page.save!
         end
       }
-      visit pageds_path + '/' + @test_paged.pid
+      visit pageds_path + '/' + test_paged.pid
       # Return page 3's prev page
       page3.next_page = next_page
       page3.save!
@@ -63,7 +58,7 @@ describe 'For page listing' do
       # Find page 3 and remove next page
       next_page = ''
       page3 = ''
-      @test_paged.pages.each {|page|
+      test_paged.pages.each {|page|
         if page.logical_number == "Page 3"
           page3 = page
           next_page = page.next_page
@@ -71,18 +66,12 @@ describe 'For page listing' do
           page.save!
         end
       }
-      visit pageds_path + '/' + @test_paged.pid
+      visit pageds_path + '/' + test_paged.pid
       # Return page 3's prev page
       page3.next_page = next_page
       page3.save!
       expect(page).to have_css('div.alert-error')
     end
-  end
-  
-  after(:all) do  
-    @test_paged.pages.each {|page| page.delete }
-    @test_paged.reload
-    @test_paged.delete
   end
 
 it 'stores a new XML datastream'
@@ -91,21 +80,13 @@ end
 
 describe 'For page reordering' do
 
-  before(:all) do
-    @test_paged = create(:paged, :with_pages)
-  end
+  let!(:test_paged) { FactoryGirl.create(:paged, :with_pages) }
 
   context "when pages are reordered" do  
     it "should respond to reorder"
     it "should accept a list of pages that need to have their order reset" 
     it "should save the logical position of each of the pages from the list"
     it "should calculate and save previous and next siblings for each page"
-  end
-
-  after(:all) do  
-    @test_paged.pages.each {|page| page.delete }
-    @test_paged.reload
-    @test_paged.delete
   end
 
 end
