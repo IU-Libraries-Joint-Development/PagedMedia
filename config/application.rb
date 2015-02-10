9705167ade9d9391db3252efcdade20ca5448bdf
Mark H. Wood <mwood@IUPUI.Edu>
2015-02-10 09:55:42 -0500

    [HPT-361] Quiet another deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module PagedMedia
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # Anticipate coming new default behavior for unsupported locales.
+    I18n.config.enforce_available_locales = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
