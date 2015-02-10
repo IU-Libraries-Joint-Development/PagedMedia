918bd3ed06ae189adcfe569ad5d2c73f18938ba0
Mark H. Wood <mwood@IUPUI.Edu>
2015-02-10 15:46:13 -0500

    [HPT-361] Start writing test of drag/drop page reordering.
    Not working yet.  It turns out that this is really hard to do because
    Javascript sortables are fragile somehow.

--- spec/features/pageds_spec.rb
+++ spec/features/pageds_spec.rb
@@ -1,3 +1,5 @@
+# Despite its name, this is a functional test of pageds_controller *and its supporting cast*.
+=begin
 describe 'For page listing' do
   let!(:test_paged) { FactoryGirl.create :paged, :with_pages }
   let(:page3) { test_paged.pages.sort { |a, b| a.logical_number <=> b.logical_number }[2] } 
@@ -5,10 +7,10 @@ describe 'For page listing' do
   context "when pages are listed" do  
     specify "they should be ordered according to prev and next page ids" do
       visit pageds_path + '/' + test_paged.pid
-      page.body.index("Page 1").should < page.body.index("Page 2")
-      page.body.index("Page 2").should < page.body.index("Page 3")
-      page.body.index("Page 3").should < page.body.index("Page 4")
-      page.body.index("Page 4").should < page.body.index("Page 5")
+      page.body.index("Page 1").should be < page.body.index("Page 2")
+      page.body.index("Page 2").should be < page.body.index("Page 3")
+      page.body.index("Page 3").should be < page.body.index("Page 4")
+      page.body.index("Page 4").should be < page.body.index("Page 5")
     end
   end
   
@@ -25,7 +27,7 @@ describe 'For page listing' do
     end
   end
 
-  context "when a infinite loop would occur " do
+  context "when an infinite loop would occur " do
     before(:each) do
       # Point page 3's next_page to itself
       page3.next_page = page3.pid
@@ -54,16 +56,25 @@ describe 'For page listing' do
 it 'stores a new XML datastream'
 
 end
-
-describe 'For page reordering' do
+=end
+feature 'User reorders pages', js: true do
 
   let!(:test_paged) { FactoryGirl.create(:paged, :with_pages) }
 
-  context "when pages are reordered" do  
-    it "should respond to reorder"
-    it "should accept a list of pages that need to have their order reset" 
-    it "should save the logical position of each of the pages from the list"
-    it "should calculate and save previous and next siblings for each page"
+  scenario "by dragging and dropping in the page order list" do
+    visit pageds_path + '/' + test_paged.pid
+    sortablePages = page.all(:xpath, "//ul[@id='sortable_pages']/li")
+    p sortablePages
+    # find a likely page, drag it across another and drop it.
+    sortablePages[1].drag_to(sortablePages[0])
+    # check the page order list
+    visit pageds_path + '/' + test_paged.pid
+    sortablePages = page.all(:xpath, "//ul[@id='sortable_pages']/li")
+    p sortablePages
   end
+
+  scenario "accepts a list of pages that need to have their order reset"
+  scenario "saves the logical position of each of the pages from the list"
+  scenario "calculates and saves previous and next siblings for each page"
 
 end
