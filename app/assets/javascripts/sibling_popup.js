e01b3d2c9455e1b8d0d396851f7b2b0e0251b480
Mark H. Wood <mwood@IUPUI.Edu>
2015-09-15 16:58:49 -0400

    [HPT-570] Try just redirecting to the existing object New pages

--- app/assets/javascripts/sibling_popup.js
+++ app/assets/javascripts/sibling_popup.js
@@ -1,3 +1,5 @@
+// Run this in the dialog popup to make a specific object type's form visible.
+// FIXME defunct
 function show_form(type) {
     divs = document.getElementsByClassName('popup_alternate_div');
     for (i = 0; i < divs.length; i++) {
@@ -7,7 +9,17 @@ function show_form(type) {
     document.getElementById('form_for_' + type).style.display = 'inherit';
 };
 
-function show_sibling_popup() {
-    window.open("/sibling_popup/show", "_blank",
+// Run this in the parent window to create the dialog popup.
+function show_sibling_popup(parent, prev_sib, next_sib) {
+    window.open("/sibling_popup/show?parent=" + parent + "&prev_sib=" + prev_sib + "next_sib=" + next_sib,
+                "_blank",
                 "location=no,menubar=no,status=no,toolbar=no,height=300,width=400");
+}
+
+// Run this in the dialog popup to redirect the parent window to a new-object page.
+function redirect_parent(type, parent, prev_sib, next_sib) {
+    opener.location=(type + "s/new"
+        + "?parent=" + parent
+        + "&prev_sib=" + prev_sib
+        + "&next_sib=" + next_sib);
 }
